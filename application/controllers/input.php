4399a246029bfe19cac86cbad337c457ad49aecc
benson000 <sbensona77@gmail.com>
2018-10-25 11:39:16 +0700

    ready to use

--- application/controllers/input.php
+++ application/controllers/input.php
@@ -1,13 +1,16 @@
 <?php
 class Input extends CI_Controller{
-	function __construct(){
+	public $channelAccessToken = '6ES9a2Db5ADmE63Eu5ntGsfse8naMUR67MSJMM95kUHMpQw7PITgJo5IzayEQaw38HfybQn6txyfTIH9Ax3U/2TzdFk8X7q/lxAO8fBSNBJW4D3Hm+3KztROvFWGXYoe44HJXI422T2ucBdgxGcCrAdB04t89/1O/w1cDnyilFU=';
+
+	public function __construct(){
 		parent::__construct();
 
-		$this->load->helper('url');
+		$this->load->helper(array('form', 'url'));
+
 		$this->load->model('models');
 	}
 	
-	function index(){
+	public function index(){
 		$data = array(
 			'status' => '' //status input
 		);
@@ -15,49 +18,210 @@ class Input extends CI_Controller{
 		$this->load->view('input_berita', $data);
 	}
 
-	function execute(){
-		$table = 'posting';
+	public function bot($berita){
+		$dbFilePath = 'line-db.json'; // user info database file path
 
-		$judul = $this->input->post('judul');
+		if (!file_exists($dbFilePath)) {
+		   file_put_contents($dbFilePath, json_encode(['user' => []]));
+		}
+		$db = json_decode(file_get_contents($dbFilePath), true);
 
-		$berita_lengkap = $this->input->post('berita_lengkap');
-		$berita_singkat = substr(
-			$berita_lengkap, //substring from here 
-			0, //starts from char number ...
-			50 //quantity of char taken
-		);
+		$bodyMsg = file_get_contents('php://input');
 
-		/*$path_foto = 'C:\xampp\htdocs\codeigniter\berita\uploaded_files\ ';
+		file_put_contents('log.txt', date('Y-m-d H:i:s') . 'Recive: ' . $bodyMsg);
+
+		$obj = json_decode($bodyMsg, true);
+
+		file_put_contents('log.txt', print_r($db, true));
+
+		foreach ($obj['events'] as &$event) {
+
+		   $userId = $event['source']['userId'];
+
+		   if (!isset($db['user'][$userId])) {
+		           $db['user'][$userId] = [
+		               'userId' => $userId,
+		               'timestamp' => $event['timestamp']
+		           ];
+		           file_put_contents($dbFilePath, json_encode($db));
+		           $message = $berita;
+		   }
+
+		   // Make payload
+		   $payload = [
+		       'replyToken' => $event['replyToken'],
+		       'messages' => [
+		           [
+		               'type' => 'text',
+		               'text' => $message
+		           ]
+		       ]
+		   ];
+
+		   // Send reply API
+		   $ch = curl_init();
+		   curl_setopt($ch, CURLOPT_URL, 'https://api.line.me/v2/bot/message/reply');
+		   curl_setopt($ch, CURLOPT_POST, true);
+		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+		   curl_setopt($ch, CURLOPT_HTTPHEADER, [
+		       'Content-Type: application/json',
+		       'Authorization: Bearer ' . $this->channelAccessToken
+		   ]);
+		   $result = curl_exec($ch);
+		   curl_close($ch);
+		}
+	}
+
+	public function push($berita){
+		$userIds = []; 
+		 
+		$url = null; 
+		$invalidurl = false; 
+		if(@simplexml_load_file($url)){ 
+		    $feeds = null; 
+		}else{ 
+		    $invalidurl = true; 
+		    $balas = "Invalid RSS feed URL"; 
+		} 
+		 
+		$i = 0; 
+		if(!empty($feeds)){ 
+		    $site = $feeds->channel->title; 
+		    $sitelink = $feeds->channel->link; 
+		 
+		    $balas = $berita; 
+		    foreach ($feeds->channel->item as $item) { 
+		        $title = $item->title; 
+		        $link = $item->link; 
+		        $description = $item->description; 
+		        $postDate = $item->pubDate; 
+		        $pubDate = date('D, d M Y',strtotime($postDate)); 
+		 
+		        if($i>=5) break; 
+		                 
+		            $balas .= "<a href='$link'>$title</a></h2>"; 
+		            $balas .= "<span>$pubDate</span>"; 
+		            $balas .= "<div>"; 
+		            $balas .= implode(' ', array_slice(explode(' ', $description), 0, 20)) . "...";  
+		            $balas .= "<a href='$link'>Read more</a>"; 
+		            $balas .= "</div>"; 
+		            $i++; 
+		    } 
+		         
+		}else{ 
+		    $balas = "<b>Berita baru sayangku :* </b> kamu harus cobain nih, artikel yang gak berguna shitpost yang cuman bikin kamu emosi dan jomblo selamanya (shiny)"; 
+		} 
+		 
+		$message = $balas; 
+		$dbFilePath = __DIR__ . '/line-db.json';  // user info database file path 
+		 
+		// open json database 
+		if (!file_exists($dbFilePath)) { 
+		   file_put_contents($dbFilePath, json_encode(['user' => []])); 
+		} 
+		$db = json_decode(file_get_contents($dbFilePath), true); 
+		 
+		if (count($db['user']) === 0) { 
+		   echo 'No user login.'; 
+
+		   exit(1); 
+		} else { 
+		   foreach ($db['user'] as &$userInfo) { 
+		       $userIds[] = $userInfo['userId']; 
+		   } 
+		} 
+		 
+		// make payload 
+		$payload = [ 
+		   'to' => $userIds, 
+		   'messages' => [ 
+		       [ 
+		           'type' => 'text', 
+		           'text' => $message 
+		       ] 
+		   ] 
+		]; 
+		 
+		// Send Request by CURL 
+		$ch = curl_init(); 
+		curl_setopt($ch, CURLOPT_URL, 'https://api.line.me/v2/bot/message/multicast'); 
+		curl_setopt($ch, CURLOPT_POST, true); 
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); 
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); 
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload)); 
+		curl_setopt($ch, CURLOPT_HTTPHEADER, [ 
+		   'Content-Type: application/json', 
+		   'Authorization: Bearer ' . $this->channelAccessToken 
+		]); 
+		$result = curl_exec($ch); 
+		curl_close($ch); 
+	}
+
+	public function execute(){
+		$path_foto = '../berita/uploaded_files/';
+
 		$config['upload_path'] = $path_foto;
+		$config['source_image'] = $path_foto.
 		$config['allowed_types'] = 'jpg|png|gif|doc|docx|pdf';
-		$config['max_size'] = 1000;
-		$config['max_width'] = 1024;
-		$config['max_height'] = 1000;
+		$config['max_size'] = 10000;
+		$config['max_width'] = 4024;
+		$config['max_height'] = 4000;
 
 		$this->load->library('upload', $config);
 
-		$file = $this->upload->data();
-		$img_name = $file['file_name'];
-		*/
+		$this->upload->initialize($config);
 
-		$date_created = date('Y-m-d G:i:s');
+		if(!$this->upload->do_upload('upload')){ //not uploading
+			$error = array('status' => $this->upload->display_errors());
 
-		$data = array(
-			'judul' => $judul,
-			'berita_singkat' => $berita_singkat,
-			'berita_lengkap' => $berita_lengkap,
-			'path_foto' => 'C:\xampp\htdocs\codeigniter\berita\uploaded_files\ ',
-			'date_created' => $date_created
-		);
+			$this->load->view('input_berita', $error);
+		}else{
+			$data = array('upload_data' => $this->upload->data());
 
-		//execute input to database
-		$this->models->insert_data($data, $table);
+			$table = 'posting';
 
-		$status = array(
-			'status' => 'Posting berita sukses!'
-		);
+			$judul = $this->input->post('judul');
 
-		$this->load->view('input_berita', $status);
+			$berita_lengkap = $this->input->post('berita_lengkap');
+			$berita_singkat = substr(
+				$berita_lengkap, //substring from here 
+				0, //starts from char number ...
+				50 //quantity of char taken
+			);
+
+			$file_info = $this->upload->data();
+			$file_name = $file_info['file_name'];
+
+			$path_foto = $path_foto.$this->upload->data('file_name');
+
+			$date_created = date('Y-m-d G:i:s');
+
+			$data = array(
+				'judul' => $judul,
+				'berita_singkat' => $berita_singkat,
+				'berita_lengkap' => $berita_lengkap,
+				'path_foto' => $path_foto,
+				'date_created' => $date_created
+			);
+
+			//push to line
+			//$this->push($berita_lengkap);
+
+			//execute input to database
+			$this->models->insert_data($data, $table);
+
+			$status = array(
+				'status' => 'Posting berita sukses!'.$file_name
+			);
+
+			$this->load->view('input_berita', $status);
+		}
 	}
 }
 ?>
--- application/views/input_berita.php
+++ application/views/input_berita.php
@@ -3,7 +3,7 @@
 	<br>
 	<br>
 	<br>
-	<form action="<?php echo base_url().'input/execute' ?>" method="post">
+	<form action="<?php echo base_url().'input/execute' ?>" method="post" enctype="multipart/form-data">
 		<div class="form-group">
 			<h2><label>Judul berita</label></h2>
 			<br>
@@ -11,10 +11,22 @@
 		</div>
 		
 		<br>
+	
+		<div>
+			<?php echo form_open_multipart('input/execute');?>
+			<input type="file" name="upload" />
+
+			<input type="submit" value="upload" />
+		</div>
+
+		<br>
 
 		<div class="form-group">
 			<textarea name="berita_lengkap" rows="30" cols="100" placeholder="Berita" class="form-control" style="font-size: 1.5em;"></textarea>
 		</div>
+		
+		<?php echo $status."<br/>"; ?>
+
 		<button class="btn btn-primary" type="Submit"> Submit</button>
 	</form>
 </div>
